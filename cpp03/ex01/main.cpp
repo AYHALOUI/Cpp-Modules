e9c60235a6d4c41e1e57ef83884dfd1ef6909a92
AYHALOUI <aymenhaloui00@gmail.com>
2023-09-19 12:42:20 +0100

    add some modification to ex01 and ex03 cpp03

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ahaloui <ahaloui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 13:12:06 by ahaloui           #+#    #+#             */
-/*   Updated: 2023/09/18 15:30:42 by ahaloui          ###   ########.fr       */
+/*   Updated: 2023/09/19 12:40:09 by ahaloui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,23 @@
 
 int main()
 {
-	ScavTrap a("ahaloui");
-	ScavTrap b("bhaloui");
-	ScavTrap c(a);
+	// ScavTrap a("ahaloui");
+	// ScavTrap b("bhaloui");
+	// ScavTrap c(a);
 
-	a.attack("bhaloui");
-	b.takeDamage(20);
-	b.attack("ahaloui");
-	a.takeDamage(20);
-	a.guardGate();
-	b.guardGate();
-	c.guardGate();
+	// a.attack("bhaloui");
+	// b.takeDamage(20);
+	// b.attack("ahaloui");
+	// a.takeDamage(20);
+	// a.guardGate();
+	// b.guardGate();
+	// c.guardGate();
+
+	// ScavTrap a("ahaloui");
+	ClapTrap *ptr = new ScavTrap("ayemen");
+
+	ptr->attack("hallui");
+
+	delete ptr;
 	return (0);
 }
