a0edc1460d0cf8d6e4e32cc6c470afb96e36ad50
AYHALOUI <aymenhaloui00@gmail.com>
2023-09-23 17:26:00 -0400

    add some modification

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -12,27 +12,9 @@
 
 #include "Fixed.hpp"
 
-// int add(int x, int y)
-// {
-//     return x + y;
-// }
 
-// int main()
-// {
-//     int x = 5 ;
-//     int value = add(x, ++x) ; // undefined behavior: is this 5 + 6, or 6 + 6?
-//     // It depends on what order your compiler evaluates the function arguments in
-
-//     std::cout << value << '\n'; // value could be 11 or 12, depending on how the above line evaluates!
-
-//     return 0;
-// }
 int main()
 {
-	// Point point(3.0, 4.0, 5.0);
-	// std::cout << point.getX() << " " << point.getY() << " " << point.getZ() << '\n';
-	// Fixed a(2.2f);
-	// std::cout << a << std::endl;
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 	std::cout << a << std::endl;
