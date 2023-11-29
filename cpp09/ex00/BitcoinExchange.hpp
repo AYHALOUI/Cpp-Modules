8e5fd94ec0739f58f12d09ef63db6ac57fcd8e63
AYHALOUI <aymenhaloui00@gmail.com>
2023-11-29 22:54:18 +0100

    cpp09

---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,20 @@
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <string>
+# include <iomanip>
+# include <sstream>
+# include <fstream>
+# include <vector>
+# include <map>
+# include <algorithm>
+# include <cmath>
+# include <ctime>
+# include <cstdlib>
+
+class BitcoinExchange
+{
+    
+};
+#endif
