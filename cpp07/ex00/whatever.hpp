9459f6aa2630310e636188ab87dd29d39a796a39
AYHALOUI <aymenhaloui00@gmail.com>
2023-11-29 21:50:45 +0100

    add some cpp07

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -5,6 +5,7 @@
 # include <string>
 
 template <typename T>
+
 void	swap(T &a, T &b)
 {
     T tmp = a;
@@ -13,14 +14,12 @@ void	swap(T &a, T &b)
 }
 
 template <typename T>
-
 T		min(T const &a, T const &b)
 {
     return (a < b ? a : b);
 }
 
 template <typename T>
-
 T		max(T const &a, T const &b)
 {
     return (a > b ? a : b);
---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,45 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+# include <string>
+
+# include <cstdlib>
+
+template <typename T>
+
+class Array
+{
+    private:
+        T	*_array;
+        int	_size;
+
+    public:
+        Array(void) : _array(NULL), _size(0) {}
+        Array(unsigned int n) : _array(new T[n]), _size(n) {}
+        Array(Array const &src) : _array(NULL), _size(0) { *this = src; }
+        ~Array(void) { delete [] _array; }
+
+        Array	&operator=(Array const &src)
+        {
+            if (this != &src)
+            {
+                delete [] _array;
+                _array = new T[src._size];
+                for (int i = 0; i < src._size; i++)
+                    _array[i] = src._array[i];
+                _size = src._size;
+            }
+            return (*this);
+        }
+
+        T	&operator[](int i)
+        {
+            if (i < 0 || i >= _size)
+                throw std::exception();
+            return (_array[i]);
+        }
+
+        int	size(void) const { return (_size); }
+};
+#endif
