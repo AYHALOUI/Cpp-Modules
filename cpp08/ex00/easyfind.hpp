d6ce87ea366c3298a3521be06ae4945203d0f831
AYHALOUI <aymenhaloui00@gmail.com>
2023-11-29 22:16:53 +0100

    cpp08 [ex00]

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,17 @@
+#ifndef EASTFIND_HPP
+# define EASTFIND_HPP
+
+# include <iostream>
+# include <algorithm>
+# include <vector>
+
+template <typename T>
+int	easyfind(T &container, int n)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), n);
+    if (it == container.end())
+        throw std::exception();
+    return (*it);
+}
+
+#endif
