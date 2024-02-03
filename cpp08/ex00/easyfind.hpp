1f503b8fb8cedc69ff3e1a4cc19da4bcb54fc9ad
aymene haloui <35409550+AYHALOUI@users.noreply.github.com>
2024-02-03 13:05:48 +0100

    add template

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,12 +6,13 @@
 # include <vector>
 
 template <typename T>
+
 int	easyfind(T &container, int n)
 {
+
     typename T::iterator it = std::find(container.begin(), container.end(), n);
     if (it == container.end())
         throw std::exception();
     return (*it);
 }
-
 #endif
