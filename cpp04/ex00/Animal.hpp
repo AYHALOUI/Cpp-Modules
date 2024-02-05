d49dcd6d237a457e141b71dcb1fce54be0a2b897
aymene haloui <35409550+AYHALOUI@users.noreply.github.com>
2024-02-05 11:14:46 +0100

    add some modification

--- cpp04/ex00/Animal.hpp
+++ cpp04/ex00/Animal.hpp
@@ -26,6 +26,7 @@ class Animal
         Animal(const Animal &copy);
         Animal &operator = (const Animal &copy);
         virtual ~Animal();
+
         
         std::string getType() const;
         virtual void makeSound() const;
