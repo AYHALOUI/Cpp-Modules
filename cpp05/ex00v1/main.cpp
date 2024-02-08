1b7c3228ada90619add3f3ddc51e02cb230b6420
aymene haloui <35409550+AYHALOUI@users.noreply.github.com>
2024-02-08 20:30:55 +0100

    add some codes

--- cpp05/ex00v1/main.cpp
+++ cpp05/ex00v1/main.cpp
@@ -2,22 +2,7 @@
 
 int main()
 {
-
-    float f = 2.5;
-    double d = 2.111111111111111111;
-
-    (void)f;
-    (void)d;
-    std::string name = "aymene";
-    std::string& ref = name;
-
-
-    std::cout << name << std::endl;
-    std::cout << ref << std::endl;
-
-    std::cout << "|" << static_cast<char>(788) << "|" << std::endl;
-    int b = static_cast<int>(f);
-    std::cout << "b = " << b << std::endl;
+    
     return (0);
 }
 
--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -15,6 +15,7 @@ class Array
         int	_size;
 
     public:
+
         Array(void) : _array(NULL), _size(0) {}
         Array(unsigned int n) : _array(new T[n]), _size(n) {}
         Array(Array const &src) : _array(NULL), _size(0) { *this = src; }
@@ -44,4 +45,5 @@ class Array
 
         int	size(void) const { return (_size); }
 };
+
 #endif
