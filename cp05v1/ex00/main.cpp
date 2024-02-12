335e6ec74a700420f7aa4a22558161efcdd2b4de
aymene haloui <35409550+AYHALOUI@users.noreply.github.com>
2024-02-12 20:31:46 +0100

    add main

--- cp05v1/ex00/main.cpp
+++ cp05v1/ex00/main.cpp
@@ -1,17 +1,25 @@
 #include "BureauCrat.hpp"
+
+void test(int x)
+{
+    if (x == 0)
+        throw BureauCrat::GradeTooHighException();
+    else
+        throw BureauCrat::GradeTooLowException();
+}
 int main()
 {
-    std::cout << "*************************" << std::endl;
-    BureauCrat b1("aymene", 1);
-    // BureauCrat b2("haloui", 150);
-
-    // try
-    // {
-    //     b1.incrementGrade();
-    // }
-    // catch(...)
-    // {
-    //     std::cout << "Caught" << std::endl;
-    // }
-    
+    try
+    {
+        test(0);
+    }
+    catch (BureauCrat::GradeTooHighException &e)
+    {
+        std::cout << "Caught an exception: " << e.what() << std::endl;
+    }
+    catch (BureauCrat::GradeTooLowException &e)
+    {
+        std::cout << "Caught an exception: " << e.what() << std::endl;
+    }
+    return (0);
 }
