2c905fa4c4f377a4998ca22bc79b3670d12df16f
AYHALOUI <aymenhaloui00@gmail.com>
2024-04-16 23:13:09 +0100

    begin of cpp08

--- cpp08v1/ex00/easyfind.hpp
+++ cpp08v1/ex00/easyfind.hpp
@@ -6,7 +6,16 @@
 /*   By: ahaloui <ahaloui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 22:33:30 by ahaloui           #+#    #+#             */
-/*   Updated: 2024/04/16 22:33:31 by ahaloui          ###   ########.fr       */
+/*   Updated: 2024/04/16 22:40:50 by ahaloui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+
+#endif
--- cpp08v1/ex00/main.cpp
+++ cpp08v1/ex00/main.cpp
@@ -6,13 +6,40 @@
 /*   By: ahaloui <ahaloui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 22:33:02 by ahaloui           #+#    #+#             */
-/*   Updated: 2024/04/16 22:33:07 by ahaloui          ###   ########.fr       */
+/*   Updated: 2024/04/16 23:12:06 by ahaloui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "easyfind.hpp"
 
 int main()
 {
-    std::cout << "Hello" << std::endl;
+    // std::vector<int> arr;
+    // std::vector<int>::iterator ptr;
+
+    
+    // int i = -1;
+    // while (++i < 5)
+    //     arr.push_back(i);
+    // for (ptr = arr.begin(); ptr < arr.end(); ptr++)
+    // {
+    //     std::cout << &(*ptr) << std::endl;
+    //     std::cout << *ptr << std::endl;   
+    // }
+
+    std::vector<int> arr;
+    int i = 0;
+    while (++i <= 5)
+    {
+        arr.push_back(i);
+        std::cout << i << " ";   
+    }
+    std::cout << std::endl;
+    std::vector<int>::iterator ptr = arr.begin();
+    //std::vector<int>::iterator ftr = arr.end();
+    // std::advance(ptr, 4);
+    // std::cout << *ptr << std::endl;
+    auto it = next(ptr, 3);
+    std::cout << *it << std::endl;
+    return (0);
 }
