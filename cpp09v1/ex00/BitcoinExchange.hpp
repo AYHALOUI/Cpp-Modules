d943d2ac2ebe938b8f0a8d6d7c8d4c1c0a3d8c97
AYHALOUI <aymenhaloui00@gmail.com>
2024-04-20 20:17:42 +0100

    cpp09 is doing

---
+++ cpp09v1/ex00/BitcoinExchange.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahaloui <ahaloui@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/20 20:12:27 by ahaloui           #+#    #+#             */
+/*   Updated: 2024/04/20 20:12:28 by ahaloui          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <string>
+# include <iomanip>
+# include <sstream>
+# include <fstream>
+# include <vector>
+# include <map>
+# include <algorithm>
+# include <cmath>
+# include <ctime>
+# include <cstdlib>
+
+class BitcoinExchange
+{
+    
+};
+#endif
---
+++ cpp09v1/ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahaloui <ahaloui@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/20 20:12:34 by ahaloui           #+#    #+#             */
+/*   Updated: 2024/04/20 20:12:36 by ahaloui          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stdio.h"
+#include "iostream"
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string *ptr = &str;
+    std::string &ref = str;
+
+    std::cout << "str: " << str << std::endl;
+    std::cout << "ptr: " << *ptr << std::endl;
+    std::cout << "ref: " << ref << std::endl;
+    return 0;
+}
